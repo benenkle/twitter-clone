dafb6a2d8f411ad60994e37e5244483d565db0bc
Abdellah Benammi <36098875+benenkle@users.noreply.github.com>
2023-02-02 12:30:42 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9h2YrzuLl9ZYGauJiMZ18q2pRyKSJn7k",
-  authDomain: "twitter-clone-2430a.firebaseapp.com",
-  projectId: "twitter-clone-2430a",
-  storageBucket: "twitter-clone-2430a.appspot.com",
-  messagingSenderId: "148023124241",
-  appId: "1:148023124241:web:8402989f7fa43320b16cb0",
-  measurementId: "G-52LGPS5SBZ"
+  apiKey: "PRIVATE-API-KEY",
+  authDomain: "PRIVATE-AUTH-DOMAIN",
+  projectId: "PRIVATE-PROJECT-ID",
+  storageBucket: "PRIVATE-SB",
+  messagingSenderId: "PRIVATE-MSI",
+  appId: "PRIVATE-APP-ID",
+  measurementId: "PRIVATE-MEASUREMENT-ID"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
